f2f13cc0b60a7e928e9db8df0c2fd3a5264890c9
Al Shut <alyonashustov@yandex.ru>
2020-03-04 12:55:29 +0300

    add travel time

--- resources/js/main.js
+++ resources/js/main.js
@@ -153,6 +153,27 @@ $(document).ready(function() {
         });
     });
 
+    $(".traineShedule_tips").mouseover(function() {
+        var nm = $(this).text();
+        var tip = ''
+        switch (nm){
+            case "eticket": $(this).prop('title', "This is a ticket with electronic registration. The electronic ticket that we email to you does not have to be exchanged prior to your trip. To board the train, you just have to show this electronic ticket and your passport.");
+            break;
+            case "paper ticket": tip = "Paper ticket can only be used in original. It has to be picked up at our office or delivered by FedEx";
+            break;
+            case "semi e-ticket": tip = "This is a ticket which will be sent to you by email, but which you will have to exchange for a boarding pass prior to the trip at the train station. You can exchange your semi e-ticket in a ticket booth (it should have an E-ticket sign) or at an electronic terminal at the train station. To get your boarding pass at the station and to board a train you will have to show your passport.";
+            break;
+            case "express": tip = "Express (skory) train\n<br>" +
+                "Express trains usually serve long distance routes and are quite popular among travelers. Average speed of a Skory train is from 50 to 91 km/h. These trains run regularly and make less stops than Passazhirsky trains.";
+            break;
+            case "high-speed": tip = "These are very fast trains that cover long distances in a short amount of time. Their speed is 140-200km/h. These trains do not have sleeper carriages, they have only seats. 1st class seats are similar to Business class airplane seats and 2nd class can be compared to Economy airplane seats. All of these trains are also Firmenny (branded) and offer high-quality service.";
+            break;
+            case "branded": tip = "Branded (firmenny) train<br>" +
+                "Branded trains are high standard trains. Their major amenities are: air-conditioning, convenient train schedule, shorter travel time, newer carriages, bed linen and toiletries of higher quality. Compartments for disabled people are also available on these trains. Each train has its specific interior design and a name such ‘Kama’, ‘Red Arrow’. These Russian trains typically have 1st, 2nd and 3rd class carriages and have a restaurant carriage.";
+            break;
+            }     
+        });
+
 
 
         // function(){
